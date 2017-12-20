a2ea3d772523829b8c7ed37abf64bafc1beb68b6
Kazuki Kunimoto <k21330404@gmail.com>
2017-12-20 19:43:50 +0900

    second

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>OKKKKKKKK!</Text>
+        <Text>2nd!!!!</Text>
       </View>
     );
   }
